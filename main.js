708f47a7269bb89952935523430cda92f13c5023
Lauren <laurenfowlkes@ymail.com>
2019-05-13 15:01:04 -0700

    Updated menu & intro sprites
    - The menu actually looks like a menu instead of just a meme
    - Added cool scale ups on over
    - Most of the buttons on the menu don't work but it's fine because play does
    - Switched out character sprites in intro scene so they aren't terrible cursed images from bee movie and lazytown

---
+++ main.js
@@ -0,0 +1,435 @@
+var game = new Phaser.Game(600, 400, Phaser.AUTO, 'phaser');
+
+
+var Menu = function(game) {};
+Menu.prototype = {
+    
+    preload: function() {
+        game.load.image('office', 'assets/img/tempoffice.png');
+        game.load.image('daybg', 'assets/img/officebgtemp.png');
+        game.load.image('textbar', 'assets/img/textbar.png');
+        game.load.image('avocado', 'assets/img/avocado.png');
+        game.load.image('titletext', 'assets/img/titletext.png');
+        game.load.image('opttext', 'assets/img/opttext.png');
+        game.load.image('playtext', 'assets/img/playtext.png');
+        game.load.image('creditstext', 'assets/img/creditstext.png');
+        game.load.image('quittext', 'assets/img/quittext.png');
+        game.load.spritesheet('player', 'assets/img/george.png', 48, 48);
+        
+        //Source: https://freesound.org/people/timgormly/sounds/170142/
+        game.load.audio('textaud', 'assets/audio/temptextsound.mp3');
+        //Source: https://incompetech.filmmusic.io/song/3930-isolated/
+        game.load.audio('officebgm', 'assets/audio/tempofficebgm.mp3');
+    },
+    create: function() { // Loads menu & instructions
+        game.stage.backgroundColor = '#000000'; // sets background color
+        titletext = game.add.sprite(50, 30, 'titletext');
+        titletext.scale.setTo(.75, .75); titletext.alpha = 0;
+        playtext = game.add.sprite(232, 130, 'playtext');
+        playtext.scale.setTo(.75, .75); playtext.alpha = 0; playtext.inputEnabled = true;
+        opttext = game.add.sprite(30, 200, 'opttext');
+        opttext.scale.setTo(.75, .75); opttext.alpha = 0;
+        creditstext = game.add.sprite(380, 200, 'creditstext');
+        creditstext.scale.setTo(.75, .75); creditstext.alpha = 0; creditstext.inputEnabled = true;
+        quittext = game.add.sprite(236, 300, 'quittext');
+        quittext.scale.setTo(.75, .75); quittext.alpha = 0; quittext.inputEnabled = true;
+        ALPHARATE = .03; //Rate at which things fade in/out
+        
+       
+    },
+    
+    update: function() {
+        /**** Fades in text *****/
+        if(titletext.alpha < .9)
+        {
+            titletext.alpha += ALPHARATE;
+            console.log('alpha: ' + titletext.alpha);
+        }
+        if(titletext.alpha >=.9 && playtext.alpha <= .9)
+        {
+            playtext.alpha += ALPHARATE;
+        }
+        if(playtext.alpha >= .9 && opttext.alpha <=.9)
+        {
+            opttext.alpha += ALPHARATE;
+        }
+        if(opttext.alpha >=.9 && quittext.alpha <=.9)
+        {
+            quittext.alpha += ALPHARATE;
+        }
+        if(quittext.alpha >=.9 && creditstext.alpha <=.9)
+        {
+            creditstext.alpha += ALPHARATE;
+        }
+        
+        
+        /***** Scales up on hover *****/
+        playtext.events.onInputOver.add(this.hoverPlay, this);
+        playtext.events.onInputOut.add(this.stopPlayHover, this);
+        opttext.events.onInputOver.add(this.hoverOpt, this); // for some reason this doesn't work but we'll figure out why later
+        opttext.events.onInputOut.add(this.stopOptHover, this);
+        creditstext.events.onInputOver.add(this.hoverCredits, this);
+        creditstext.events.onInputOut.add(this.stopCreditsHover, this);
+        quittext.events.onInputOver.add(this.hoverQuit, this);
+        quittext.events.onInputOut.add(this.stopQuitHover, this);
+        /**** Handles moving to different screens ****/
+        playtext.events.onInputDown.add(this.startPlay, this);
+    },
+    
+    hoverPlay: function() {
+        playtext.scale.setTo(1, 1);
+        playtext.x -= 12;
+        playtext.y -= 10;
+    },
+    
+    stopPlayHover: function() {
+        playtext.scale.setTo(.75, .75);
+        playtext.x += 12;
+        playtext.y += 10;
+    },
+    
+    hoverOpt: function() {
+        console.log('Here');
+        opttext.scale.setTo(1, 1);
+        opttext.x -= 12;
+        opttext.y -= 10;
+    },
+    
+    stopOptHover: function() {
+        opttext.scale.setTo(.75, .75);
+        opttext.x += 12;
+        opttext.y += 10;
+    },
+    hoverCredits: function() {
+        creditstext.scale.setTo(1, 1);
+        creditstext.x -= 12;
+        creditstext.y -= 10;
+    },
+    
+    stopCreditsHover: function() {
+        creditstext.scale.setTo(.75, .75);
+        creditstext.x += 12;
+        creditstext.y += 10;
+    },
+    hoverQuit: function() {
+        quittext.scale.setTo(1, 1);
+        quittext.x -= 12;
+        quittext.y -= 10;
+    },
+    
+    stopQuitHover: function() {
+        quittext.scale.setTo(.75, .75);
+        quittext.x += 12;
+        quittext.y += 10;
+    },
+    startPlay: function() {
+        game.state.start('Intro');
+    }
+}
+
+var Intro = function(game) {};
+Intro.prototype = {
+    init: function() {
+        office = game.add.sprite(0,0, 'office'); //adds office background
+        
+        //I'm intentionally changing to quotes so the script stands out
+        //Array of script strings
+        this.msgs = [
+        /*0*/ "Ah, you're here. Thanks for coming in.",
+        /*1*/"I appreciate your agency agreeing to work\nwith us, Ms ...",
+        /*2*/"I'm so sorry, remind me your name again?",
+        /*3*/"This is a line that will never be used",
+        /*4*/"My name is " + this.name,
+        /*5*/"Oh! Ms. " + this.name + ", of course!"
+                     ];
+        ///////////
+        this.pcSprite = [4]; //array of all line #s that need PC sprite on screen
+        this.bossSprite = [0, 1, 2, 5]; //array of all line #s that need the boss sprite on screen
+        this.textObj = game.add.text(10, 300, 'wow', { font: '20px Courier New', fill: '#FFF'}); //Text object used for displying typewriter script text
+        this.progtextObj = game.add.text(470, 375, '', { font: '15px Courier New', fill: '#FFF', align: 'right'});
+        this.index = 0; //which line of the script we're pulling from
+        this.i = 0; //what character of the sentence the typewriter is on
+        this.counter = 0; //counter to slow down the typewriter.
+        this.name = ''; // the name of the character, inputted later by player
+        this.inputCreated = false; //checks if the user has reached the textbox screen yet
+        this.temp; //text object to be used for instructions
+        
+    },
+    preload: function() {
+        game.load.image('boss', 'assets/img/bosstemp.png');
+        game.load.image('PC', 'assets/img/pctemp.png');
+    },
+    create: function() { // Loads game state
+        game.stage.backgroundColor = '#000'; // sets background color
+        /* Plugin source: https://github.com/azerion/phaser-input*/
+        this.input = game.add.plugin(PhaserInput.Plugin)
+        pc = game.add.sprite(-5, 90, 'PC');
+        boss = game.add.sprite(460, 89, 'boss');
+        pc.alpha = 0;
+        this.textaud = game.add.audio('textaud');
+        
+    },
+    
+    update: function() {
+        
+        //if user pressed space to progress the dialogue OR if user presseed enter on text box
+        if((game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR) || game.input.keyboard.justPressed(Phaser.Keyboard.ENTER)) && !this.inputCreated)
+        {
+            this.progtextObj.text = ''
+            if(this.i < this.msgs[this.index].length)
+            {
+                this.i = this.msgs[this.index].length;
+                this.textObj.text = this.msgs[this.index];
+                if(this.index == 0) //displays instruction on first line
+                {
+                    this.progtextObj.text = 'Press space >>'
+                }
+                else //trusts that the player now knows >> = press space
+                {
+                    this.progtextObj.x = 575
+                    this.progtextObj.text = '>>'
+                }
+            }
+            else
+            {
+            this.index++;
+            this.i = 0;
+            this.counter = 0;
+            if(this.pcSprite.includes(this.index)) //checks if we're on a line that needs the PC sprite on screen
+            {
+                pc.alpha = 1;
+                boss.alpha = 0;
+            }
+            else // we're on a line that needs boss' sprite on screen
+            {
+                pc.alpha = 0;
+                boss.alpha = 1;
+            }
+            }
+        }
+        //TODO: remove dev cheat skip key
+        if(office.alpha <= 0 || game.input.keyboard.justPressed(Phaser.Keyboard.X))
+        {
+            game.state.start('Day');
+        }
+        else if(this.index >= this.msgs.length)
+        {
+            office.alpha -= .02;
+            this.textObj.text = '';
+            this.progtextObj.text = '';
+            boss.alpha -= .02;
+            pc.alpha -= .02;
+        }
+        /**** Writes text to screen, ONLY while there are still messages in the array ****/
+        if(this.index < this.msgs.length && this.counter % 2 == 0) //displays each letter every 2 frames
+        {
+            if(this.index == 3)
+            {
+                this.textObj.text = '';
+            }
+            else if(this.i <= this.msgs[this.index].length)
+            {
+                this.typewriter(this.msgs[this.index], this.textObj, this.i); //calls typewriter function
+                this.i++; //moves us to the next letter
+            }
+            if(this.i == this.msgs[this.index].length)
+            {
+                if(this.index == 0) //displays instruction on first line
+                {
+                    this.progtextObj.text = 'Press space >>'
+                }
+                else //trusts that the player now knows >> = press space
+                {
+                    this.progtextObj.x = 575
+                    this.progtextObj.text = '>>'
+                }
+            }
+            
+        }
+        // If there are still messages left
+        if(this.index < this.msgs.length && this.i <= this.msgs[this.index].length)
+        {
+            this.counter++; //counter, updates every frame
+        }
+
+        /**** Deals with input box *****/
+        if(this.index == 3 && !this.inputCreated) //If we're waiting for text input
+        {
+            this.temp = game.add.text(180, 300, 'Enter your first name', { font: '20px Courier New', fill: '#FFF'});
+            // Adds text box to screen
+            inputBox = game.add.inputField
+            (200, 325, {
+             fill: '#000000', font: '45px Courier New', width: 200, height: 50, backgroundColor: '#FFFFFF', borderColor: '#aba000', borderWidth: 2, focusOurOnEnter: true});
+            this.inputCreated = true; //we've created the input box now, so true
+            inputBox.blockInput = false; //Ignores other keystrokes while user is typing
+            inputBox.startFocus(); //sets textbox to be active
+        }
+        // Checks if user has pressed enter on text box yet & if there's actually an active text box on screen
+        if(this.inputCreated && game.input.keyboard.justPressed(Phaser.Keyboard.ENTER))
+           {
+           this.temp.destroy();
+           inputBox.endFocus(); //sets textbox inactive
+           this.name = inputBox.value; //sets the name field to whatever the user entered
+           inputBox.destroy(); //destroys the textbox
+           this.inputCreated = false; //we no longer have an active textbox, so false
+           this.index++; //advances to the next line of dialogue
+           this.msgs[4] = "My name is " + this.name + ".";
+           this.msgs[5] = "Oh! Ms. " + this.name + ", of course!"; //updates the script with next name
+            pc.alpha = 1;
+            boss.alpha = 0;
+           }
+    },
+    // Moderately unnecessary function for typewriter text display, but it looks nice to have things as their own function so I made this.
+    typewriter: function(msg, textObj, i) {
+        //console.log(msg);
+        textObj.text = msg.substr(0, i);
+        if(this.counter % 4 == 0)
+        this.textaud.play('', 0, .125, false);
+    }
+    
+}
+
+var Day = function(game) {};
+Day.prototype = {
+    
+
+    create: function() { //Creates the day stage
+        cursors = game.input.keyboard.createCursorKeys();
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#000'; // sets background color
+        game.world.setBounds(0, 0, 1600, 1000);
+        daybg = game.add.sprite(0, 0, 'daybg');
+        daybg.alpha = 0;
+        this.officebgm = game.add.audio('officebgm');
+        this.officebgm.play('', 0, .5, true);
+        player = game.add.sprite(50, 165, 'player');
+        textbar = game.add.sprite(0, 286, 'textbar');
+        player.scale.x = 1.5; player.scale.y = 1.5;
+        game.physics.arcade.enable(player);
+        player.animations.add('left', [1, 5, 9, 13], 10, true);
+        player.animations.add('right', [3, 7, 11, 15], 10, true);
+        player.animations.add('up', [2, 6, 10, 14], 10, true);
+        player.animations.add('down', [0, 4, 8, 12], 10, true);
+        //player.enableBody = true;
+        player.body.collideWorldBounds = true;
+        i = 0; //placeholder for typewriter
+        textObj = game.add.text(10, 300, '', { font: '20px Courier New', fill: '#FFF'});
+        msg = 'You can move around the office\nusing the arrow keys or WASD.';
+        counter = 0; // frame counter
+        moved = false; // checks if the move message was destroyed yet
+        activeText = false; //checks if the user is in the middle of a dialogue spot
+        objs = game.add.group();
+        objs.enableBody = true;
+        avo1 = objs.create(800, 200, 'avocado');
+        avo2 = objs.create(700, 400, 'avocado');
+        avo3 = objs.create(1000, 600, 'avocado');
+        avo1.body.immovable = true;
+        avo2.body.immovable = true;
+        avo3.body.immovable = true;
+        game.physics.arcade.enable(objs);
+        SPEED = 100; //dev speed constant, default is 100
+        
+    },
+    
+    update: function() {
+        // Fade in BG
+        if(daybg.alpha < 1)
+        {
+            daybg.alpha += .025;
+        }
+        else if(daybg.alpha >= 1 && counter % 2 == 0 && !moved && i <= msg.length)
+        {
+            textObj.text = msg.substr(0, i);
+            i++;
+            counter++;
+        }
+        else if(daybg.alpha >= 1 && !moved)
+        {
+            counter++;
+        }
+        
+        if(moved && textObj.alpha >= 0)
+        {
+            textObj.alpha -= .025;
+            textbar.alpha -= .025;
+        }
+        game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN);
+        //textbar.x = game.camera.x; textbar.y = game.camera.y + 286;
+        //lets the player move with WASD or arrow keys
+        if(!activeText) //deals with player movement, but only if they're not in the middle of a text sequence
+        {
+            if(cursors.left.isDown || game.input.keyboard.isDown(Phaser.Keyboard.A))
+            {
+                player.body.velocity.x = SPEED * -1;
+                player.animations.play('left');
+                if(!moved)
+                {
+                    moved = true;
+                }
+            }
+            else if(cursors.right.isDown || game.input.keyboard.isDown(Phaser.Keyboard.D))
+            {
+                player.body.velocity.x = SPEED;
+                player.animations.play('right');
+                if(!moved)
+                {
+                    moved = true;
+                }
+            }
+            else if(cursors.up.isDown || game.input.keyboard.isDown(Phaser.Keyboard.W))
+            {
+                player.body.velocity.y = SPEED * -1;
+                player.animations.play('up');
+                if(!moved)
+                {
+                    moved = true;
+                }
+            }
+            else if(cursors.down.isDown || game.input.keyboard.isDown(Phaser.Keyboard.S))
+            {
+                player.body.velocity.y = SPEED;
+                player.animations.play('down');
+                if(!moved)
+                  {
+                    moved = true;
+                  }
+            }
+            else
+            {
+                player.animations.stop();
+                player.body.velocity.x = 0;
+                player.body.velocity.y = 0;
+            }
+        }
+        else //deals with dialogue when text sequence is active
+        {
+        
+        }
+        
+        game.physics.arcade.collide(player, objs);
+    }
+}
+    
+var Battle = function(game) {};
+Battle.prototype = {
+    create: function() { //
+        game.stage.backgroundColor = '#facade'; // sets background color
+        game.add.text(0, 100, 'hahahah u done fucked up', { font: '30px Courier New', fill: '#000', align:'center' });
+        
+    },
+    
+    update: function() {
+        
+    }
+}
+
+
+
+
+
+//Adds game states
+game.state.add('Menu', Menu); //adds intro state
+game.state.add('Intro', Intro); // adds play state
+game.state.add('Day', Day); //adds main menu state
+game.state.add('Battle', Battle); // adds battle state
+game.state.start('Menu'); // starts the game at the main menu
